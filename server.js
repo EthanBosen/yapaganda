078857db57493ac45e9af6e1d57ffdbd58004a46
Ethan Bosen <ethbosen@gmail.com>
2024-06-09 00:48:35 -0400

    revert

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
+app.get('/:page', (req, res) => {
+    const page = req.params.page;
+    const validPages = ['index', 'merch', 'directory']; 
+    if (validPages.includes(page)) {
+        res.sendFile(path.join(__dirname, `${page}.html`));
+    } else {
+        res.status(404).send('Page not found');
+    }
+});
+
+app.get('/search/:query', (req, res) => {
+    const query = req.params.query.toUpperCase();
+    res.send(`You searched for: ${query}`);
 });
 
 app.listen(PORT, () => {
