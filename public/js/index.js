847c5a602d85bb8348220767be658e91a6cfd47d
Ethan Bosen <ethbosen@gmail.com>
2024-06-17 04:37:38 -0400

    ios fitting

--- public/js/index.js
+++ public/js/index.js
@@ -16,33 +16,6 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = '/directory'; 
     });
 
-    // Function to initialize YouTube players
-    function onYouTubeIframeAPIReady() {
-        videoContainers.forEach(function(container, index) {
-            let videoId = container.querySelector('iframe').getAttribute('src').split('/')[4]; 
-            players[index] = new YT.Player(container, {
-                videoId: videoId,
-                events: {
-                    'onReady': onPlayerReady,
-                    'onStateChange': onPlayerStateChange
-                }
-            });
-        });
-    }
-
-    // Autoplay next video when the current one ends
-    function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.ENDED) {
-            // Move to the next video
-            currentVideoIndex++;
-            if (currentVideoIndex >= players.length) {
-                currentVideoIndex = 0; 
-            }
-            players[currentVideoIndex].playVideo();
-        }
-    }
-
-    // YouTube API script insertion
     let tag = document.createElement('script');
     tag.src = "https://www.youtube.com/iframe_api";
     let firstScriptTag = document.getElementsByTagName('script')[0];
