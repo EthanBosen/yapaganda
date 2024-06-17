3eedfe23ca33adfa7d10f70f587ade113b7b0eec
Ethan Bosen <ethbosen@gmail.com>
2024-06-17 13:20:42 -0400

    index.js clean up

--- public/js/index.js
+++ public/js/index.js
@@ -16,15 +16,45 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = '/directory'; 
     });
 
+    let bannerVideo = document.getElementById('banner-video');
+    if (bannerVideo) {
+        bannerVideo.playbackRate = 0.5;
+    }
+
+
+    // YouTube IFrame API
     let tag = document.createElement('script');
     tag.src = "https://www.youtube.com/iframe_api";
     let firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-
-    document.getElementById('banner-video').playbackRate = 0.5;
 });
 
+let players = [];
+let currentVideoIndex = 0;
 
 function onYouTubeIframeAPIReady() {
+    const videoContainers = document.querySelectorAll('.video-container');
+    videoContainers.forEach((container, index) => {
+        const iframe = container.querySelector('iframe');
+        players[index] = new YT.Player(iframe, {
+            events: {
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    });
+}
+
+function onPlayerStateChange(event) {
+    if (event.data === YT.PlayerState.ENDED) {
+        playNextVideo();
+    }
+}
+
+function playNextVideo() {
+    if (players.length > 0) {
+        const nextVideoIndex = (currentVideoIndex + 1) % players.length;
+        players[currentVideoIndex].pauseVideo();
+        players[nextVideoIndex].playVideo();
+        currentVideoIndex = nextVideoIndex;
+    }
 }
