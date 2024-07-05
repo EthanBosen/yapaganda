6ac9e181bb56e5c478a63f0cf37a6d5c63d83cdc
Ethan Bosen <ethbosen@gmail.com>
2024-07-05 14:48:27 -0400

    search function for merch

--- public/js/directory.js
+++ public/js/directory.js
@@ -38,3 +38,21 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href = '/directory'; 
     });
 });
+
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('artistList');
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName('a')[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
+}
--- public/js/index.js
+++ public/js/index.js
@@ -58,3 +58,21 @@ function playNextVideo() {
         currentVideoIndex = nextVideoIndex;
     }
 }
+
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('artistList');
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName('a')[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = '';
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
+}
