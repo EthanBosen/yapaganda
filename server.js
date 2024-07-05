9411fce7784b2bce7f05b4573b9f65003ce58974
Ethan Bosen <ethbosen@gmail.com>
2024-07-05 15:32:52 -0400

    search function for homepage

--- server.js
+++ server.js
@@ -29,11 +29,14 @@ app.listen(PORT, () => {
 app.get('/carousel-data', (req, res) => {
     const carouselItems = [
         { id: 1, image: '/public/media/bluetshirtpreview.jpg', title: 'Blue T-Shirt Collection', link: 'https://bluetshirt.com/collections/all' },
-        { id: 2, image: '/public/media/taylorgangpreview.jpg', title: 'Taylor Gang Merchandise', link: 'https://store.taylorgang.com/' },
-        { id: 3, image: '/public/media/jetlifeapparelpreview.jpg', title: 'Jet Life Apparel', link: 'https://jetlifeapparel.com/' },
-        { id: 4, image: '/public/media/midnightorganicpreview.jpg', title: 'Midnight Organic', link: 'https://midnightorganic.com/' },
-        { id: 5, image: '/public/media/problemvjasonpreview.jpg', title: 'Problem vs Jason', link: 'https://shopjasonmartin.com/' },
-        { id: 6, image: '/public/media/ETSpreview.jpg', title: 'ETS', link: 'https://theetsofficial.com/collections/ets' },
+        { id: 2, image: '/public/media/midnightorganicpreview.jpg', title: 'Midnight Organic', link: 'https://midnightorganic.com/' },
+        { id: 3, image: '/public/media/ETSpreview.jpg', title: 'ETS', link: 'https://theetsofficial.com/collections/ets' },
+        { id: 4, image: '/public/media/taylorgangpreview.jpg', title: 'Taylor Gang Merchandise', link: 'https://store.taylorgang.com/' },
+        { id: 5, image: '/public/media/jetlifeapparelpreview.jpg', title: 'Jet Life Apparel', link: 'https://jetlifeapparel.com/' },
+        { id: 6, image: '/public/media/media/domkennedypreview.jpg.jpg', title: 'OPM', link: 'https://opmopm.us/' },
+        { id: 7, image: '/public/media/djquikpreview.jpg', title: 'DJ Quik', link: 'https://www.djquikstore.com/' },
+        { id: 8, image: '/public/media/problemvjasonpreview.jpg', title: 'Problem vs Jason', link: 'https://shopjasonmartin.com/' },
+        { id: 9, image: '/public/media/jay305preview.jpg', title: 'Jay 305', link: 'https://jay305.komi.io' },
     ];
     res.json(carouselItems);
 });
