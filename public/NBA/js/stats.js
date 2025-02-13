b77317c49af60201894b992a6166ae17dbd8948b
Ethan Bosen <ethbosen@gmail.com>
2025-02-12 21:14:11 -0500

    teams manually sorted based on record Feb 12

--- public/NBA/js/stats.js
+++ public/NBA/js/stats.js
@@ -2,24 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const homeBtn = document.getElementById('index-btn');
     const merchBtn = document.getElementById('merch-btn');
     const statsBtn = document.getElementById('stats-btn');
-    const nflBtn = document.getElementById('nfl-btn');
-    const teamList = document.getElementById('teamList');
+    const nbaBtn = document.getElementById('nba-btn');
     const bannerVideo = document.querySelector('video');
 
     homeBtn.addEventListener('click', function() {
         window.location.href = '/index';
     });
 
-    nflBtn.addEventListener('click', function() {
-        window.location.href = '/public/NFL/nfl.html';
+    nbaBtn.addEventListener('click', function() {
+        window.location.href = '/public/NBA/nba.html';
     });
 
     statsBtn.addEventListener('click', function() {
-        window.location.href = '/public/NFL/stats.html';
+        window.location.href = '/public/NBA/stats.html';
     });
 
     merchBtn.addEventListener('click', function() {
-        window.location.href = '/public/NFL/merch.html';
+        window.location.href = '/public/NBA/merch.html';
     });
     
     // Video playback rate
